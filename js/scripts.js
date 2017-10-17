1325901bec4ebc5e95d5721054b172d627d48383
AndrewWaller <awalldrew2@gmail.com>
2017-10-16 17:38:11 -0700

    added back timerout thingy

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
-var pathToGenerateImg = "../img/snapcode.svg"; // path to generate
-var pathToLoadImg = "../img/SC.png"; // path to load img
+let pathToGenerateImg = "../img/snapcode.svg"; // path to generate
+let pathToLoadImg = "../img/SC.png"; // path to load img
 
 function showLoad(){
     document.getElementById('Generate1').src = "../img/SC.png";
+    setTimeout(showCode, 1000);
 }
 
 function showCode(){
