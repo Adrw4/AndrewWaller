7554d5c8ce1aad3240cac835be735d602148f8d5
AndrewWaller <awalldrew2@gmail.com>
2017-10-16 17:23:33 -0700

    removed timeout from tooltip

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@ var pathToLoadImg = "../img/SC.png"; // path to load img
 
 function showLoad(){
     document.getElementById('Generate1').src = "../img/SC.png";
-    setTimeout(showCode, 5000);
 }
 
 function showCode(){
