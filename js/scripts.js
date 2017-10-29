15c4111f7815afecdeda2a963a22b4ce0705880c
AndrewWaller <awalldrew2@gmail.com>
2017-10-28 22:57:24 -0700

    going to redo all javasript on the site

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
-let pathToGenerateImg = "../img/snapcode.svg"; // path to generate
+function snapchatPngFlip() {
+    document.getElementById("snapchatLogo").src = "../img/FB.png";
+};
+
+
+
+
+
+
+
+
+
+/*let pathToGenerateImg = "../img/snapcode.svg"; // path to generate
 let pathToLoadImg = "../img/SC.png"; // path to load img
 
 function showLoad(){
     document.getElementById('Generate1').src = "../img/SC.png";
-    setTimeout(showCode, 200);
 }
 
 function showCode(){
-    document.getElementById('Generate1').src = "../img/snapcode.svg" ;
-}
+    document.getElementById('Generate1').src = "../img/snapcode.svg";
+}*/
